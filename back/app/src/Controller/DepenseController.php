36f992efca9d12b47de1ab19f2e06342522eaea5
vitolinho <93949338+vitolinho@users.noreply.github.com>
2023-01-11 16:37:54 +0100

    update

--- back/app/src/Controller/DepenseController.php
+++ back/app/src/Controller/DepenseController.php
@@ -8,23 +8,53 @@ use App\Entity\Depense;
 
 class DepenseController extends AbstractController {
 
-    public function ajtDepense()
+    public function createDepense()
     {
-        echo 'Je suis la fonction ajtDepense !!!';
+        session_start();
+        $id = $_SESSION['id'];
+
+        $montant = $_POST['montant'];
+        $description = $_POST['description'];
+        $preuve = $_FILES;
+        move_uploaded_file($_FILES['preuve']['tmp_name'], dirname(__DIR__, 2) . '/uploads/' . $_FILES['preuve']['name']);
+        $datetime = new \DateTime();
+
+        $newDepense = (new Depense())
+                ->setMontant($montant)
+                ->setDescription($description)
+                ->setPreuve($preuve)
+                ->setDatetime($datetime);
+        
+        $manager = new CollocationManager(new PDOFactory());
+        $manager->insertDepense($newDepense);
+        #Ajouter colocationId et userId et les set
     }
 
-    public function suprDepense()
-    {
-        echo 'Je suis la fonction suprDepense !!!';
-    }
 
     public function modifDepense()
     {
-        echo 'Je suis la fonction modifier une depense !!!';
-    }
+        session_start();
+        $id = $_SESSION['id'];
 
-    public function recapDepense()
+        $montant = $_POST['montant'];
+        $description = $_POST['description'];
+        $preuve = $_POST['preuve'];
+        $datetime = new \DateTime();
+
+        $newDepense = (new Depense())
+                ->setMontant($montant)
+                ->setDescription($description)
+                ->setPreuve($preuve)
+                ->setDatetime($datetime);
+        
+        $manager = new CollocationManager(new PDOFactory());
+        $manager->updateDepense($newDepense);
+    } #colocationId et userId
+
+    public function suprDepense()
     {
-        echo 'Je suis la fonction récapitulatif de toutes les depenses !!!';
+        $id = (int)$_POST['depenseId'];
+        $manager = new DepenseManager(new PDOFactory());
+        $manager->deleteDepense($id);
     }
 }
