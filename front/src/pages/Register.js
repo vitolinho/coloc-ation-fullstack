1bee0c261c18c5f1388a3e682e0642ecaf7715a4
Arsène DOBROVOLSKYY <arsened.devweb@gmail.com>
2023-01-12 13:52:07 +0100

    register json

--- front/src/pages/Register.js
+++ front/src/pages/Register.js
@@ -1,9 +1,44 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './Register.css'
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Register.css";
 
 export const Register = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const user = {
+    usernameJSON: username,
+    passwordJSON: password,
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(JSON.stringify(user));
+    let reponse = await fetch('http://localhost:2345/login',
+      {
+        method: 'POST',
+        credentials: 'include',
+        headers: new Headers({
+          "Content-type": "application/json"
+        }),
+        body: JSON.stringify(user),
+        mode: 'cors'
+      }
+    )
+      .then(e => reponse.json())
+      .then(json => console.log(json))
+  }
+
+  const handleInputUsername = (e) => {
+    setUsername(e.target.value)
+  }
+  const handleInputMail = (e) => {
+    setPassword(e.target.value)
+  }
+  const handleInputPassword = (e) => {
+    setPassword(e.target.value)
+  }
+
   return (
     <div className="registerContainer">
       <div className="backgroundImage"></div>
@@ -11,17 +46,20 @@ export const Register = () => {
         <div className="formWrapper">
           <span className="logo">Coloc'Ation</span>
           <span className="title">Register</span>
-          <form>
-            <input type="text" placeholder="username" />
-            <input type="email" placeholder="email" />
-            <input type="password" placeholder="password" />
+          <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="username" id="username" name="username" onChange={handleInputUsername} />
+            <input type="email" placeholder="email" id="email" name="email" onChange={handleInputMail} />
+            <input type="password" placeholder="password" id="password" name="password" onChange={handleInputPassword} />
             <button>Sign Up</button>
           </form>
           <p>
-            You already have an account? <Link className='linkColor' to={"/"}>Login</Link>
+            You already have an account?{" "}
+            <Link className="linkColor" to={"/"}>
+              Login
+            </Link>
           </p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
